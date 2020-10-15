0f6d6a3ac07587d41929a97c61ce04fac730a1ad
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-10-15 21:03:52 +0200

    ending basics server s settings

--- server.js
+++ server.js
@@ -6,20 +6,6 @@ const knex = require('knex');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const db = knex({
   client: 'pg',
   connection: {
@@ -27,78 +13,33 @@ const db = knex({
     user: 'postgres',
     password: '1gb78AK74-',
     database: 'smart-brain'
-  },
+  }
 });
-/*
-db.select('*').from('users')
-  .then(data => {
 
-  });
-*/
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'john',
-      password: 'apple',
-      email: 'john@gmail.com',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: '124',
-      name: 'sally',
-      password: 'ananas',
-      email: 'sally@gmail.com',
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-  /*,
-  login: [
-    {
-      id: '',
-      hash: '',
-      email: 'john@gmail.com'
-    }
-  ]*/
-};
 
-
-
-
-
-
-
-
-
-
-
-/**
- * Giving an idea....
- *
+/**  Giving an idea....
  *
  * CHECK TEST:
  *   --> res = GET req OK
  *
- * SIGNIN:
+ * SIGN IN:
  *    --> POST = success OR fail
  * REGISTER:
  *    -->  POST = user object
- * /PROFILE/:userId
+ * 
+ *   /PROFILE/:userId
  *    --> GET = user
- *
- * /IMAGE:
+ *   /IMAGE:
  *    --> PUT return user object
  */
 
 app.get('/', (req, res) => {
-  res.send(database.users);
+  res.send('success');
 });
 
 /**
@@ -106,21 +47,51 @@ app.get('/', (req, res) => {
  *  we focus on the Body
  *  then select the raw & JSON on postman.co
  * 
- *  ==> testing the bcrypt npm package  
+ *  ==> testing the bcrypt npm package 
+ * 
+ * 
+ *  bcrypt-nodejs => npm package
+ *                
+ *   => look at the notice on "bcrypt-vs-bcrypt.js"
+ *   => https://github.com/kelektiv/node.bcrypt.js/wiki/bcrypt-vs-bcrypt.js
+ * 
+ * 
 **/
 app.post('/signin', (req, res) => {
-  bcrypt.compare('annanas', '$2a$10$4qFPQyxl74THxsCDw9WeAeT1mI4Z7ECBrmTEgdIqGccS.npKIHznW', function (err, res) {
-    console.log('first guess', res);
-  });
-  bcrypt.compare('veggies', '$2a$10$4qFPQyxl74THxsCDw9WeAeT1mI4Z7ECBrmTEgdIqGccS.npKIHznW', function (err, res) {
-    console.log('scd guess', res);
-  });
+  
+  /**   
+   *  ======> No transaction here , Not needed
+  *     because we just checking and 
+  *        NOT modifying any data 
+  **/
 
-  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-    res.json(database.users[0]); 
-  } else {
-    res.status(400).json('We ve got an error, pass do not match!???');
-  }
+  db
+    .select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      //console.log(isValid);
+
+      if(isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            //console.log(user);
+            res.json(user[0]);
+          })
+          .catch(error => res.status(400).json('unable to get user'));
+      } else {
+
+        res.status(400).json('wrong credentials');
+
+      }
+
+    })
+    .catch(error => res.status(400).json('wrong credentials'));
+
 });
 
 /**
@@ -128,22 +99,43 @@ app.post('/signin', (req, res) => {
  *  the new user inputs (name, email, pass)
  *  we want to  push another one
  *  new user in the database variable (object)
- *
- *  (start first the "static" way to do)
- */
+ * 
+ *  TRANSACTIONS CONCEPT:
+ * => join code blocks on db 
+ * => for multiples operations on db
+ *     if one fail , then all fail 
+ * ===> joining login and users
+ * 
+ **/
 
 app.post('/register', (req, res) => {
-  const { email, name } = req.body;
+  const { email, name, password } = req.body;
+  
+  /* ====> transaction from knex package   */
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
 
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date()
+    trx.insert({
+      hash: hash,
+      email: email
     })
-  .then(user => {
-    res.json(user[0]);
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+
+      return trx('users')
+        .returning('*')
+        .insert({
+          email: loginEmail[0],
+          name: name,
+          joined: new Date()
+        })
+        .then(user => {
+          res.json(user[0]);
+        });
+    })
+    .then(trx.commit)
+    .catch(trx.rollback);
   })
   .catch(error => res.status(400).json("unable to register"));
 });
@@ -156,22 +148,23 @@ app.get('/profile/:id', (req, res) => {
   db
     .select('*')
     .from('users')
-    .where({
-      id: id
-    })
-    .then(user => {
-      if(user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json('Profile Not Found!');
-      }
-    })
+    .where(
+      {
+        id: id
+      })
+    .then(user => 
+      {
+        if(user.length) {
+          res.json(user[0]);
+        } else {
+          res.status(400).json('Profile Not Found!');
+        }
+      })
     .catch(error => res.status(400).json('Error getting profile.'));
 
 });
 
 /**
- *
  * In that case,
  *  we update from the image section url
  *  cos' we want to increment entries
@@ -179,68 +172,36 @@ app.get('/profile/:id', (req, res) => {
  *
  * Notice, here we want
  *  to check on the body of the req
- *
  */
 app.put('/image', (req, res) => {
   const { id } = req.body;
 
   db('users')
+  
     .where('id', '=', id)
     .increment('entries', 1)
     .returning('entries')
-    .then(entries => {
-      res.json(entries[0]);
-    })
+    .then(entries => 
+      {
+        res.json(entries[0]);
+      })
     .catch(error => res.status(400).json('unable to get entries'));
-
-
 
 });
 
 
 
-/*
-  let found = false;
-
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-
-  if (!found) {
-    res.status(404).json('There are no such user with that name. Impossible to count.');
-  }*/
 
 
 
-/**
- *  bcrypt-nodejs => npm package
- *                
- *   => look at the notice on "bcrypt-vs-bcrypt.js"
- *   => https://github.com/kelektiv/node.bcrypt.js/wiki/bcrypt-vs-bcrypt.js
- * 
- */
-// Load hash from your password DB.
 
-//bcrypt.hash(password, null, null, function(err, hash) {
-// console.log(hash);
-//});
-//bcrypt.compare("bacon", hash, function(err, res) {
-// res == true
-//});
-//bcrypt.compare("veggies", hash, function(err, res) {
-// res = false
-//});
 
 
 
 
 
 /**
- *  .listen on prot 3000
+ *  .listen on port 3000
  *  & confirm with console.log
  */
 app.listen(3000, () => {
@@ -248,13 +209,10 @@ app.listen(3000, () => {
 });
 
 
-/**
+/**    on terminal :
  * 
- * on terminal :
- * 
- * run => 'npm run start' command
+ * run => 'npm start' command
  * 
  * will launch the command set on package.json
  *     => 'node server.js'
- * 
- */
+**/
