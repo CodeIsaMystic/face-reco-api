073d0338a5bc7374bc69586a9dfdbd29c020eef5
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-10-19 17:04:59 +0200

    creating and testing db

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-defined-44273',
     user: 'postgres',
     password: '1gb78AK74-',
     database: 'smart-brain'
